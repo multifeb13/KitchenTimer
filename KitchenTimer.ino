eb3dfcf5ed85b8ac219805e9c90302170225ad3c
multifeb13 <musicfair@gmail.com>
2020-05-27 22:27:04 +0900

    one shot timer

--- KitchenTimer.ino
+++ KitchenTimer.ino
@@ -1,20 +1,25 @@
 #include <MsTimer2.h>
 
-#define BLINK_INTERVAL_MS   (500)
+#define ONE_SHOT_MS   (3000)  /* Timer for OneShot(ms)   */
+#define INTERVAL_MS   (500)   /* Timer for Interrupt(ms) */
 
-void flash( )
+void oneshot( )
 {
-  static boolean output = HIGH;
+  static unsigned short interval_count = 0;
 
-  digitalWrite( LED_BUILTIN, output );
-  output = !output;
+  interval_count++;
+  if( interval_count == ( ONE_SHOT_MS / INTERVAL_MS ) ){
+    /* Time for OneShot */
+    digitalWrite( LED_BUILTIN, HIGH );
+    MsTimer2::stop( );
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
   pinMode( LED_BUILTIN, OUTPUT );
-
-  MsTimer2::set( BLINK_INTERVAL_MS, flash );
+    
+  MsTimer2::set( INTERVAL_MS, oneshot );
   MsTimer2::start( );
 }
 
