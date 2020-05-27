b377b1a70dac400235c28cb5f9bdb0c3fdb8f9a8
multifeb13 <musicfair@gmail.com>
2020-05-27 22:00:56 +0900

    flash interval by MsTimer2

--- KitchenTimer.ino
+++ KitchenTimer.ino
@@ -1,6 +1,21 @@
+#include <MsTimer2.h>
+
+#define BLINK_INTERVAL_MS   (500)
+
+void flash( )
+{
+  static boolean output = HIGH;
+
+  digitalWrite( LED_BUILTIN, output );
+  output = !output;
+}
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode( LED_BUILTIN, OUTPUT );
 
+  MsTimer2::set( BLINK_INTERVAL_MS, flash );
+  MsTimer2::start( );
 }
 
 void loop() {
